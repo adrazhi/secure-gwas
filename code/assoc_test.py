c71ef14e1cb197949e259a1097a99b8d7d96fbc0
Shreyan <shreyan@mit.edu>
2020-04-24 20:41:04 -0700

    added more evaluation metrics

--- code/assoc_test.py
+++ code/assoc_test.py
@@ -5,6 +5,8 @@ f1 = sys.argv[1]
 f2 = sys.argv[2]
 x1 = np.loadtxt(f1)
 x2 = np.loadtxt(f2)
+print("Top 10 Positions for X1: ", x1.argsort()[-10:][::-1])
+print("Top 10 Positions for X2: ", x2.argsort()[-10:][::-1])
 print("Average Absolute Difference: ", np.average(np.abs(x1 - x2)))
 print("Correlation: ", np.corrcoef(x1, x2)[0, 1])
 print("Distance: ", np.linalg.norm(x1 - x2))
