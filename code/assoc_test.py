aa10481eec6d90453dc01936f955e6d85cc98234
Shreyan <shreyan@mit.edu>
2020-04-27 20:53:13 -0700

    added fuzzy evaluation of association values

--- code/assoc_test.py
+++ code/assoc_test.py
@@ -1,10 +1,28 @@
 import sys
 import numpy as np
 
-f1 = sys.argv[1]
-f2 = sys.argv[2]
-x1 = np.loadtxt(f1)
-x2 = np.loadtxt(f2)
+pref1 = sys.argv[1]
+pref2 = sys.argv[2]
+x1 = np.loadtxt(pref1 + "_assoc.txt")
+x2 = np.loadtxt(pref2 + "_assoc.txt")
+
+if (x1.shape != x2.shape):
+	gkeep1 = np.loadtxt(pref1 + "_gkeep1.txt") & np.loadtxt(pref1 + "_gkeep2.txt")
+	gkeep2 = np.loadtxt(pref2 + "_gkeep1.txt") & np.loadtxt(pref2 + "_gkeep2.txt")
+	int ind1 = 0
+	int ind2 = 0
+	for int i in range(gkeep1.shape[0]):
+		if gkeep1[i] == 1 and gkeep2[i] == 1:
+			ind1 += 1
+			ind2 += 1
+		elif gkeep1[i] == 1:
+			x1 = np.delete(x1, ind1)
+		elif gkeep2[i] == 1:
+			x2 = np.delete(x2, ind2)
+
+print("len of x1: ", x1.shape[0])
+print("len of x2: ", x2.shape[0])
+
 print("Top 10 Positions for X1: ", x1.argsort()[-10:][::-1])
 print("Top 10 Positions for X2: ", x2.argsort()[-10:][::-1])
 print("Average Absolute Difference: ", np.average(np.abs(x1 - x2)))
